c6faa0e698890446c87d8aabe09e42941a093080
jimaomao@e-eduspace.com <jimaomao@e-eduspace.com>
2021-01-04 14:56:29 +0800

    i18n版本更新

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <!-- <h1>{{ msg }}</h1> -->
     <button>{{$t('login.login')}}</button>
     <div>{{$t('login.reset')}}</div>
     <button @click="change">切换</button>
